0d70a95e508d1b80e2451536ebbcc890dda483f7
relkimm <bbonggu92@gmail.com>
2022-10-21 19:43:12 +0900

    chore: holiday api 디렉토리 생성 및 이동

--- src/external/holiday/api/index.ts
+++ src/external/holiday/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import AppConfig from "../../config";
+import AppConfig from "../../../config";
 import { GetHolidaysRequest, GetHolidaysResponse } from "./model";
 
 interface HolidayApi {
---
+++ src/external/holiday/api/model.ts
@@ -0,0 +1,39 @@
+import dayjs from "dayjs";
+
+export interface GetHolidaysRequest {
+  year: number;
+  limit?: number;
+}
+
+export interface GetHolidaysResponse {
+  response: {
+    header: {
+      resultCode: string;
+      resultMsg: string;
+    };
+    body: {
+      items: {
+        item: HolidayItemResponse[];
+      };
+      numOfRows: number;
+      pageNo: number;
+      totalCount: number;
+    };
+  };
+}
+
+export interface HolidayItemResponse {
+  seq: number;
+  locdate: number;
+  dateKind: string;
+  isHoliday: boolean;
+  dateName: string;
+}
+
+export function parseLocdate(locdate: number): Date {
+  const locdateString = locdate.toString();
+  const year = locdateString.substring(0, 4);
+  const month = locdateString.substring(4, 6);
+  const day = locdateString.substring(6, 8);
+  return new Date(`${year}-${month}-${day}`);
+}
