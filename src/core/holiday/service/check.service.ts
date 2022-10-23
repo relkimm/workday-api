ff3414ef15c9e6f076bd88b4a22074680bcc81cc
relkimm <bbonggu92@gmail.com>
2022-10-23 20:38:54 +0900

    fix: Holiday Checker 날짜 비교 계산 로직 수정

--- src/core/holiday/service/check.service.ts
+++ src/core/holiday/service/check.service.ts
@@ -1,10 +1,11 @@
+import { isSameDate } from "../../../shared/util/date";
 import { HolidayChecker } from "./checker";
 import { getHolidayReader, HolidayReader } from "./reader";
 
 function HolidayCheckService(holidayReader: HolidayReader): HolidayChecker {
   async function isHoliday(date: Date): Promise<boolean> {
     const holidays = await holidayReader.get();
-    return holidays.some((holiday) => holiday.date === date);
+    return holidays.some((holiday) => isSameDate(holiday.date, date));
   }
 
   return {
--- src/shared/util/date.ts
+++ src/shared/util/date.ts
@@ -4,3 +4,11 @@ export function isWeekend(date: Date) {
   const dayOfWeek = date.getDay();
   return weekendValues.some((weekendValue) => weekendValue === dayOfWeek);
 }
+
+export function isSameDate(a: Date, b: Date): boolean {
+  return (
+    a.getFullYear() === b.getFullYear() &&
+    a.getMonth() === b.getMonth() &&
+    a.getDate() === b.getDate()
+  );
+}
